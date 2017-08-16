ed3b5d82a7eced604791a926af4588cb1dabb7cd
hodoug <j.hodoug@gmail.com>
2017-08-16 12:47:01 +0900

    처음 커밋

---
+++ dev/src/App.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+import {render} from 'react-dom';
+import PLFitting from './components/PLFitting';
+
+render(
+	<PLFitting />,
+	document.getElementById('root')
+);
